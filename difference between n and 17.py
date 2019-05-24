4ab8219d6907e7ca6b8666d6e1f631e7a4c7b1b4
KashafSyeda <50900045+KashafSyeda@users.noreply.github.com>
2019-05-24 23:50:32 +0500

    Add files via upload

---
+++ difference between n and 17.py
@@ -0,0 +1,8 @@
+print("the difference between a given number and 17\tdifference cannot be negative")
+n = int(input("enter a number: "))
+if n>17:
+    m = n - 17
+    print(m)    
+else:
+    m = 17 - n
+    print(m)
