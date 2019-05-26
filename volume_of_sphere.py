fc33269ef7cc8b3004fff55600780738f8946d9f
KashafSyeda <50900045+KashafSyeda@users.noreply.github.com>
2019-05-26 05:37:43 +0500

    Update volume_of_sphere.py

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -1,4 +1,4 @@
 print("Python program to get the volume of a sphere")
-r = int(input("enter radius: "))
+r = float(input("enter radius: "))
 v = 4/3 *(3.14*(r*r*r))
 print("volume of sphere is " + str(v))
