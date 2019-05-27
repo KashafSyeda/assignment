666488f957aabeaa0684e7847931d6b01cb4af3d
KashafSyeda <50900045+KashafSyeda@users.noreply.github.com>
2019-05-27 06:45:55 +0500

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,6 @@
+print("whether letter is a vowel or Consonant")
+c = str(input("enter any alphabet: "))
+if(c=='A' or c=='a' or c=='E' or c =='e' or c=='I' or c=='i' or c=='O' or c=='o' or c=='U' or c=='u'):
+    print(c + " is a Vowel")
+else:
+    print(c + " is a Consonant")  
